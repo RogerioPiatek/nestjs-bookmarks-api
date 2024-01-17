5794d076fbf3747b2e487c6ef281d9abe551bc02
Rogerio <rogeriopiatek@gmail.com>
2024-01-17 01:28:30 -0300

    feat: ensure signin logic is done correctly

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signup() {
-    return { msg: 'I have signed up' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    //generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    //save the new user in the DB
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      //return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
