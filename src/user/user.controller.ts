432b0295bb3098a2adfe38c40d2a3d4d1da4310d
Rogerio <rogeriopiatek@gmail.com>
2024-01-19 22:34:47 -0300

    feat: ensure editUser patch request is possible

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from 'src/auth/guard';
 import { User } from '@prisma/client';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private userService: UserService) {}
   @Get('me')
   getMe(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
   }
 }
